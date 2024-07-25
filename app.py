0457bc11184e67ac2be1d94ad886cb845029d14a
fskanaya <22611113@students.uii.ac.id>
2024-07-25 20:57:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("Aplikasi Prediksi Tarif Uber")
 
 # Deskripsi aplikasi
 st.write("""
-    Aplikasi ini menggunakan model Random Forest untuk mengklasifikasikan tarif Uber menjadi 'high' atau 'low'.
+    Aplikasi ini menggunakan model Random Forest untuk mengklasifikasikan tarif Uber menjadi 'tinggi' atau 'rendah'.
     Masukkan detail perjalanan untuk mendapatkan hasil prediksi.
 """)
 
@@ -26,7 +26,9 @@ if st.button("Prediksi"):
     input_data = np.array([[pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude]])
     
     # Melakukan prediksi menggunakan model
-    prediction = model.predict(input_data)
-
-    # Menampilkan hasil prediksi
-    st.write(f"Hasil prediksi: {prediction[0]} (high: tarif tinggi, low: tarif rendah)")
+    try:
+        prediction = model.predict(input_data)
+        # Menampilkan hasil prediksi
+        st.write(f"Hasil prediksi: {prediction[0]} (tinggi: tarif tinggi, rendah: tarif rendah)")
+    except Exception as e:
+        st.error(f"Terjadi kesalahan: {e}")
